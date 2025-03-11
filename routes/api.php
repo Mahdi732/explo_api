7d317a4d730a94a158eb86e8294da3aa6ba0c2ed
Mahdi732 <ey449751@gmail.com>
2025-03-11 13:03:09 +0000

    add api routing for register and login

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,6 +19,9 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/test', function(){
-    return 'test'; 
-});
+Route::post('/', [UserController::class, 'index']);
+
+Route::post('/register', [UserController::class, 'register']);
+
+Route::post('/login', [UserController::class, 'login']);
+
