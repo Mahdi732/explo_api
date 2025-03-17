2c9e247b46c20b0e83ef154d7fbb0a10656b8155
Mahdi732 <ey449751@gmail.com>
2025-03-17 15:43:13 +0000

    push

---
+++ database/migrations/2025_03_14_144347_destinations.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('destinations', function (Blueprint $table){
+            $table->string('name');
+            $table->string('categorie');
+            $table->string('activities');
+            $table->foreignId('itinerary_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
